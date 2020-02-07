9857dbb08bfd4b633891b8b9d8c987c7a9edd4c6
hbyyy <qjaduddl94@gmail.com>
2020-02-07 17:39:37 +0900

    feat: serializer.py 작성, view 생성

--- app/quickstart/views.py
+++ app/quickstart/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.contrib.auth.models import User, Group
 # Create your views here.
+from rest_framework import viewsets
+
+from quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    user 를 확인하거나 편집할 수 있는  API endpoint
+    """
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Group을 확인하거나 편집할 수 있는 API endpoint
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
